5dc008f5ecae6e37e44b6987a6e36c420466ada9
Shinmera <shinmera@tymoon.eu>
2014-02-23 20:21:25 +0100

    Reworked dictionary to use word class and methods

--- dictionary.lisp
+++ dictionary.lisp
@@ -15,13 +15,31 @@ WORD        ::= ((TERM+) . (TRANSLATION+))
 TERM        --- a string
 TRANSLATION --- a string"
   `(setf (gethash ,(string-downcase name) *dictionaries*)
-         ',words))
+         (mapcar #'(lambda (cons)
+                     (make-instance 'word :terms (car cons) :translations (cdr cons)))
+                 ',words)))
+
+(defgeneric (setf dictionary) (words name))
+(defgeneric (setf term-translations) (translations dictionary term))
+(defgeneric (setf term-siblings) (siblings dictionary term))
+(defgeneric terms (word-or-dictionary))
+(defgeneric translations (word-or-dictionary))
+(defgeneric translation-p (word-or-dictionary translation))
+
+(defclass word ()
+  ((%terms :initarg :terms :initform () :accessor terms)
+   (%translations :initarg :translations :initform () :accessor translations))
+  (:documentation "A word."))
+
+(defmethod print-object ((word word) stream)
+  (print-unreadable-object (word stream :type T)
+    (format stream "~{~s~^, ~}" (terms word)))
+  word)
 
 (defun dictionary (name)
   "Retrieve the dictionary with NAME."
   (gethash (string name) *dictionaries*))
 
-(defgeneric (setf dictionary) (words name))
 (defmethod (setf dictionary) (words name)
   (setf (gethash (string name) *dictionaries*) words))
 
@@ -31,7 +49,7 @@ TRANSLATION --- a string"
 
 (defun copy-dictionary (name)
   "Creates a deep list-copy of the dictionary."
-  (mapcar #'(lambda (a) (cons (copy-list (car a)) (copy-list (cdr a)))) (dictionary name)))
+  (copy-list (dictionary name)))
 
 (defun shuffled-dictionary (name)
   "Create a shuffled copy of the dictionary."
@@ -39,27 +57,28 @@ TRANSLATION --- a string"
 
 (defun find-word (dictionary term)
   "Returns the word data that matches the given term."
-  (find term (dictionary dictionary) :key #'car :test #'string-equal))
+  (find term (dictionary dictionary) :key #'terms :test #'(lambda (term terms) (find term terms :test #'string-equal))))
 
 (defun term-translations (dictionary term)
   "Returns the list of translations for the term. See FIND-WORD."
-  (cdr (find-word dictionary term)))
+  (translations (find-word dictionary term)))
 
-(defgeneric (setf term-translations) (translations dictionary term))
 (defmethod (setf term-translations) (translations dictionary term)
-  (setf (cdr (find-word dictionary term)) translations))
+  (setf (translations (find-word dictionary term)) translations))
 
-(defun translation-p (dictionary term translation)
+(defmethod translation-p ((dictionary list) translation)
   "Returns T if the given translations is one of the possible translations for the term. See FIND-WORD."
-  (member translation (term-translations dictionary term) :test #'string-equal))
+  (member translation (dictionary dictionary) :key #'translations :test #'(lambda (translation list) (member translation list :test #'string-equal))))
+
+(defmethod translation-p ((word word) translation)
+  (member translation (translations word) :test #'string-equal))
 
 (defun term-siblings (dictionary term)
   "Returns the list of sibling terms for the term. See FIND-WORD."
-  (car (find-word dictionary term)))
+  (terms (find-word dictionary term)))
 
-(defgeneric (setf term-siblings) (siblings dictionary term))
 (defmethod (setf term-siblings) (siblings dictionary term)
-  (setf (car (find-word dictionary term)) siblings))
+  (setf (terms (find-word dictionary term)) siblings))
 
 (defun copy-term-siblings (dictionary term)
   "Create a copy of the list of siblings. See TERM-SIBLINGS."
@@ -69,13 +88,13 @@ TRANSLATION --- a string"
   "Create a copy of the list of translations. See TERM-TRANSLATIONS."
   (copy-list (term-translations dictionary term)))
 
-(defun terms (dictionary)
+(defmethod terms ((dictionary list))
   "Returns the list of term-lists of a dictionary."
-  (mapcar #'car (dictionary dictionary)))
+  (mapcar #'terms (dictionary dictionary)))
 
-(defun translations (dictionary)
+(defmethod translations ((dictionary list))
   "Returns the list of translation-lists of a dictionary."
-  (mapcar #'cdr (dictionary dictionary)))
+  (mapcar #'translations (dictionary dictionary)))
 
 (defun copy-terms (dictionary)
   "Create a copy of the terms. See TERMS."
@@ -97,14 +116,17 @@ DICTIONARY   --- A name used to find the dictionary.
 COPY         --- If non-NIL a copy of the dictionary is used as per COPY-DICTIONARY.
 SHUFFLE      --- If non-NIL a shuffled copy of the dictionary is used as per SHUFFLED-DICTIONARY.
 BODY         ::= form*"
-  `(loop for (,terms . ,translations) in ,(cond (shuffle `(shuffled-dictionary ,dictionary))
-                                                (copy `(copy-dictionary ,dictionary))
-                                                (T `(dictionary, dictionary)))
-         do (progn ,@body)))
+  (let ((word (gensym "WORD")))
+    `(loop for ,word in ,(cond (shuffle `(shuffled-dictionary ,dictionary))
+                               (copy `(copy-dictionary ,dictionary))
+                               (T `(dictionary, dictionary)))
+           for ,terms = (terms ,word)
+           for ,translations (translations ,word)
+           do (progn ,@body))))
 
 (defun add-word (dictionary terms translations)
   (setf (dictionary dictionary)
-        (cons (cons terms translations) (dictionary dictionary))))
+        (cons (make-instance 'word :terms terms :translations translations) (dictionary dictionary))))
 
 (defun add-terms (dictionary term &rest terms)
   (dolist (new-term terms)
@@ -117,7 +139,7 @@ BODY         ::= form*"
 (defun remove-word (dictionary term)
   (setf (dictionary dictionary)
         (delete term (dictionary dictionary)
-                :test #'(lambda (term list) (member term list :test #'string-equal))
+                :test #'(lambda (term word) (member term (terms word) :test #'string-equal))
                 :key #'car)))
 
 (defun remove-terms (dictionary term &rest terms)
